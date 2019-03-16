96aaa1ca65367d28151c1d7b70e93bf115ae66cc
00127417 <00127417@uca.edu.sv>
2019-03-15 19:44:23 -0600

    se hizo que cambiara la imagen cuando se toca

--- app/src/main/java/com/example/ejercicio2/MainActivity.java
+++ app/src/main/java/com/example/ejercicio2/MainActivity.java
@@ -10,7 +10,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView image1, image2, image3, image4, image5, image6, image7, image8, image9;
-    private int[] images = {R.drawable.image1, R.drawable.image2, R.drawable.image3};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,29 +43,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         int ViewId = v.getId();
+        int[] images = {R.drawable.image1, R.drawable.image2, R.drawable.image3,R.drawable.image4,R.drawable.image5,R.drawable.image6,R.drawable.image7,R.drawable.image8,R.drawable.image9};
         switch (ViewId) {
             case R.id.image1:
-                image1.setImageResource(new Random().nextInt(images.length));
+                image1.setImageResource(images[(int) Math.floor(Math.random()*9)]);
                 break;
             case R.id.image2:
+                image2.setImageResource(images[(int) Math.floor(Math.random()*9)]);
                 break;
             case R.id.image3:
+                image3.setImageResource(images[(int) Math.floor(Math.random()*9)]);
                 break;
             case R.id.image4:
+                image4.setImageResource(images[(int) Math.floor(Math.random()*9)]);
                 break;
             case R.id.image5:
+                image5.setImageResource(images[(int) Math.floor(Math.random()*9)]);
                 break;
             case R.id.image6:
+                image6.setImageResource(images[(int) Math.floor(Math.random()*9)]);
                 break;
             case R.id.image7:
+                image7.setImageResource(images[(int) Math.floor(Math.random()*9)]);
                 break;
             case R.id.image8:
+                image8.setImageResource(images[(int) Math.floor(Math.random()*9)]);
                 break;
             case R.id.image9:
+                image9.setImageResource(images[(int) Math.floor(Math.random()*9)]);
                 break;
         }
 
     }
+
+
 
 
 }
